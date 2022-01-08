73768a3620404f8bf5657825804a76764eaa0b2b
Jose Isai Reyes Hernandez <revanisai777@gmail.com>
2022-01-08 21:58:42 +0000

    Implemented Header and NewBudget cmps.

---
+++ src/components/NewBudget.jsx
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import Message from "./Message";
+
+const NewBudget = ({ budget, setBudget }) => {
+
+  const [message, setMessage] = useState('')
+
+  function handleBudget(e) {
+    e.preventDefault()
+    const numberBudgetType = Number(budget)
+
+    if (!numberBudgetType || numberBudgetType < 0) {
+      setMessage('Invalid budget...')
+      console.log(message)
+    } else {
+      console.log(numberBudgetType);
+    }
+  }
+
+  return (
+    <div className="contenedor contenedor-presupuesto sombra">
+      <form 
+        className="formulario"
+        onSubmit={handleBudget}
+      >
+        <div className="campo">
+          <label htmlFor="budget">
+            Set your Budget
+          </label>
+
+          <input 
+            className="nuevo-presupuesto"
+            type="text" 
+            id="budget"
+            value={budget}
+            onChange={(e) => {
+              setBudget(e.target.value)
+              setMessage('')
+            }}
+          />
+        </div>
+
+        <input type="submit" value="Add your budget" />
+
+        { message && <Message type="error">{message}</Message> }
+      </form>
+    </div>
+  )
+}
+
+export default NewBudget
